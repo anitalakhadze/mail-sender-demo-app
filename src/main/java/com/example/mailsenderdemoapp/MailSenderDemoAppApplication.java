667ebbe1e695378a98f21a9d6c8d9476f3bbf97e
atala15 <atala15@freeuni.edu.ge>
2022-04-27 13:53:10 +0400

    Initial commit

---
+++ src/main/java/com/example/mailsenderdemoapp/MailSenderDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.mailsenderdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailSenderDemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailSenderDemoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mailsenderdemoapp/MailSenderDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mailsenderdemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailSenderDemoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
