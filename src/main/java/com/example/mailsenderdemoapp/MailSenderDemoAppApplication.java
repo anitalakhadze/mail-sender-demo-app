35329483b6800a581facf3dc75e88bf9db605b6d
atala15 <atala15@freeuni.edu.ge>
2022-04-27 15:40:53 +0400

    Add little code to main method for testing purposes.

--- src/main/java/com/example/mailsenderdemoapp/MailSenderDemoAppApplication.java
+++ src/main/java/com/example/mailsenderdemoapp/MailSenderDemoAppApplication.java
@@ -1,13 +1,24 @@
 package com.example.mailsenderdemoapp;
 
+import com.example.mailsenderdemoapp.model.Mail;
+import com.example.mailsenderdemoapp.service.MailService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MailSenderDemoAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MailSenderDemoAppApplication.class, args);
+        Mail mail = new Mail();
+        mail.setMailFrom("hello@anita.com");
+        mail.setMailTo("medium.readers@gmail.com");
+        mail.setMailSubject("Thanks!");
+        mail.setMailContent("Thank you for reading my blogs!");
+
+        ApplicationContext ctx = SpringApplication.run(MailSenderDemoAppApplication.class, args);
+        MailService mailService = (MailService) ctx.getBean("mailServiceImpl");
+        mailService.sendEmail(mail);
     }
 
 }
